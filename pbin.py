4db092914cba141f48350b9009f2b5d97a4762f8
Andrei Duma <andrei.duma.dorian@gmail.com>
2014-01-07 11:28:37 +0200

    Created model

--- pbin.py
+++ pbin.py
@@ -9,10 +9,30 @@ db = SQLAlchemy(app)
 
 app.debug = True
 
+class Paste(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text())
+
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return '<Paste %s>' % self.id
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/paste', methods=['POST'])
+@app.route('/add', methods=['POST'])
+def add():
+    p = Paste(request.form['text'])
+    db.session.add(p)
+    db.session.commit()
+    return redirect(url_for('paste'), id=p.id)
+
+@app.route('/p/<int:id>')
 def paste():
-    return redirect(url_for('index'))
+    p = Paste.query.get(id)
+    if p is None:
+        return abort(404)
+    return p.text
